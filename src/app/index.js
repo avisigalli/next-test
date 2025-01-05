beb666835d2fd720552d5f0b1da166e385213354
Alessandro Visigalli <31890838+avisigalli@users.noreply.github.com>
2025-01-05 21:09:46 +0100

    Add files via upload

---
+++ src/app/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+    <div>
+    <h1>Benvenuto nel mio sito statico!</h1>
+    <p>Questo sito è costruito con Next.js e utilizza la 
+        generazione statica delle pagine.</p>
+    </div>
+    );
+    }
+
